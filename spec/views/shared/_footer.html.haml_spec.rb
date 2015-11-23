f9045d841b1cc92662f52b6485931a7d40918ef1
Pere Joan Martorell <perejoan@itnig.net>
2015-11-24 00:17:58 +0100

    Add view test for footer

---
+++ spec/views/shared/_footer.html.haml_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'shared/_footer' do
+  before do
+    doc = File.read(Rails.root + 'spec/support/eurofxref-hist-90d.xml')
+    stub_request(:get, /www.ecb.europa.eu/).to_return(:body => doc)
+  end
+
+  it "should display copyright and select box" do
+    render
+    copyright = Nokogiri::HTML(rendered).css('#copyright')
+
+    expect(copyright).to have_content(Time.now.year)
+    expect(rendered).to have_selector('#currency')
+  end
+end
